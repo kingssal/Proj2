adb23c7942a625107306a41fb7bf0b3a82202e3e
강성준 <ksjoon02@naver.com>
2023-11-05 09:20:10 +0900

    중간 완성본2

--- prob1/inf_int.cpp
+++ prob1/inf_int.cpp
@@ -151,40 +151,37 @@ inf_int operator+(const inf_int& a, const inf_int& b) {
     int carry = 0;
     int i = a.length - 1, j = b.length-1, k = 0;
 
-      // 두 숫자를 뒤에서부터 더함
     while (i >= 0 || j >= 0 || carry) {
         int sum = carry;
         if (i >= 0) sum += a.digits[i--] - '0';
         if (j >= 0) sum += b.digits[j--] - '0';
-        temp[k++] = (sum % 10) + '0'; // 한 자리 수를 문자로 변환하여 저장
-        carry = sum / 10; // 다음 자리수로 올림수 계산
+        temp[k++] = (sum % 10) + '0'; 
+        carry = sum / 10; 
     }
 
-    // 결과 문자열을 올바른 순서로 뒤집음
+    // turn the digits
     for (int l = 0; l < k / 2; l++) {
         char t = temp[l];
         temp[l] = temp[k - l - 1];
         temp[k - l - 1] = t;
     }
-    temp[k] = '\0'; // 문자열 종료
+    temp[k] = '\0'; 
 
-    return inf_int(temp); // 결과 문자열로 inf_int 객체 생성
+    return inf_int(temp); 
 }
 
 inf_int operator-(const inf_int& a, const inf_int& b) {
-// 두 숫자 모두 양수인 경우에만 유효
+
     if (!a.thesign || !b.thesign) {
-        // 음수 또는 혼합된 부호에 대한 처리가 필요
-        return inf_int(); // 임시적으로 0을 반환
+        
+        return inf_int(); // return 0 temporarily
     }
 
-    // 결과를 저장할 임시 문자열
-    char temp[10000]; // 충분히 큰 크기로 설정
+    char temp[10000]; // size of storage
 
-    int borrow = 0; // 빌림수
+    int borrow = 0; 
     int i = a.length - 1, j = b.length - 1, k = 0;
 
-    // 두 숫자를 뒤에서부터 빼면서 결과를 임시 문자열에 저장
     while (i >= 0 || j >= 0) {
         int diff = borrow;
         if (i >= 0) diff += a.digits[i--] - '0';
@@ -195,25 +192,22 @@ inf_int operator-(const inf_int& a, const inf_int& b) {
         } else {
             borrow = 0;
         }
-        temp[k++] = diff + '0'; // 한 자리 수를 문자로 변환하여 저장
+        temp[k++] = diff + '0'; 
     }
 
-    // 결과 문자열을 올바른 순서로 뒤집음
     for (int l = 0; l < k / 2; l++) {
         char t = temp[l];
         temp[l] = temp[k - l - 1];
         temp[k - l - 1] = t;
     }
-    temp[k] = '\0'; // 문자열 종료
+    temp[k] = '\0'; 
 
-    return inf_int(temp); // 결과 문자열로 inf_int 객체 생성
+    return inf_int(temp); 
 }
 
 inf_int operator*(const inf_int& a, const inf_int& b) {
-     // 두 숫자 모두 양수인 경우에만 유효
     if (!a.thesign || !b.thesign) {
-        // 음수 또는 혼합된 부호에 대한 처리가 필요
-        return inf_int(); // 임시적으로 0을 반환
+        return inf_int(); 
     }
 
     // 결과를 저장할 임시 배열
@@ -228,27 +222,26 @@ inf_int operator*(const inf_int& a, const inf_int& b) {
         }
     }
 
-    // 결과를 문자열로 변환
     int k = a.length + b.length;
-    while (k > 0 && temp[k - 1] == 0) k--; // 앞쪽의 0 제거
+    while (k > 0 && temp[k - 1] == 0) k--; 
 
-    if (k == 0) return inf_int("0"); // 결과가 0인 경우
+    if (k == 0) return inf_int("0"); 
 
     char result[20000];
     for (int i = 0; i < k; i++) {
         result[i] = temp[k - i - 1] + '0';
     }
-    result[k] = '\0'; // 문자열 종료
+    result[k] = '\0'; 
 
-    return inf_int(result); // 결과 문자열로 inf_int 객체 생성
+    return inf_int(result); 
 }
 
 std::ostream& operator<<(std::ostream& os, const inf_int& obj) {
      if (!obj.thesign) {
-        os << '-'; // 음수인 경우 '-' 기호를 먼저 출력
+        os << '-'; 
     }
     for (int i = 0; i < obj.length; i++) {
-        os << obj.digits[i]; // 각 자리의 숫자를 순서대로 출력
+        os << obj.digits[i]; 
     }
     return os;
 }
