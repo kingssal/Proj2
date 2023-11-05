b31e2eed5f597689997552c4d2538a15b1337c7b
강성준 <ksjoon02@naver.com>
2023-11-05 13:10:20 +0900

    오류 수정본

--- prob1/main.cpp
+++ prob1/main.cpp
@@ -8,12 +8,13 @@ using namespace std;
 
 int main()
 {
-	inf_int a, e;
+	inf_int a;
 	inf_int b(100);
 	inf_int c("321111111111122222222222233333333333444444444445555555555");
 	inf_int d("123451987651234572749499923455022211");
 	inf_int f=d;
 	inf_int g(f);
+	inf_int e("-1223123143142332131231244");
 
 	// cin >> g ;   // not required
 
@@ -28,7 +29,7 @@ int main()
 		cout << "b : " << b << endl; //~~~~~~~~44
 		cout << "c : " << c << endl; //a에서 00뺀거
 		cout << "d : " << d << endl; //~~~~~11
-		// cout << "e : " << e << endl;
+		cout << "e : " << e << endl;
 		cout << "f : " << f << endl; //same as d
 		// cout << "g : " << g << endl;
 	}
--- prob2/inf_int.cpp
+++ prob2/inf_int.cpp
@@ -1,41 +1,47 @@
 #include "inf_int.h"
 
 inf_int::inf_int(){
-    length = 1;
+    //기본값으로 0을 할당
+    length = 1; 
     thesign = true;
-    digits = new char[length+1];
-    strcpy(digits, "0");
+    digits = new char[length + 1]; // /0를 위해 +1
+    strcpy(digits, "0"); //0으로 초기화
 }
 
 inf_int::inf_int(int value){
+    //부호 확인
     if(value >= 0){
         thesign = true;
     } else {
         thesign = false;
-        value= -value;
+        value = -value; //절대값으로 일단 변환시켜 놓음
     }
 
-    char buffer[50];
+    //정수를 문자열로 변환하고 임시 버퍼를 할당
+    char buffer[100];
     sprintf(buffer, "%d", value);
 
+    //digits에 복사
     length = strlen(buffer);
-    digits = new char[length +1];
+    digits = new char[length + 1];
     strcpy(digits, buffer);
 }
 
+//문자열을 매개변수로 받는 객체
 inf_int::inf_int(const char* value){
-    if(value == NULL) {
+    //null값일때
+    if(value == NULL){
         length = 1;
         thesign = true;
-        digits = new char[length+1];
+        digits = new char[length + 1];
         strcpy(digits, "0");
         return;
     }
-
-    if(value[0]=='-'){
-        thesign=false;
-        value++;
-    } else{
+    //부호확인
+    if (value[0] == '-') {
+        thesign = false;
+        value++; // -다음 부분으로 이동시킴
+    } else {
         thesign = true;
     }
 
@@ -43,32 +49,41 @@ inf_int::inf_int(const char* value){
 
     digits = new char[length + 1];
     strcpy(digits, value);
+
+    //앞쪽의 불필요한 0을 제거해야 하는 것 필요할지도
 }
 
+//복사 생성자
 inf_int::inf_int(const inf_int& other){
     length = other.length;
     thesign = other.thesign;
 
-    digits = new char[length +1];
+    digits = new char[length+1];
     strcpy(digits, other.digits);
 }
 
+//소멸자
 inf_int::~inf_int(){
+    //동적 할당된 메모리 해제
     delete[] digits;
 }
 
 inf_int& inf_int::operator=(const inf_int& other){
+    //자기자신을 대입했으면
     if(this == &other){
         return *this;
     }
-    
+    //기존 digits메모리 해제
     delete[] digits;
+
+    //다른 객체의 멤버변수 복사
     length = other.length;
     thesign = other.thesign;
 
-    digits = new char[length +1];
+    digits = new char[length + 1]; 
     strcpy(digits, other.digits);
 
+    //현재 객체의 참조를 반환
     return *this;
 }
 
@@ -80,40 +95,46 @@ bool operator==(const inf_int& a, const inf_int& b) {
 }
 
 bool operator!=(const inf_int& a, const inf_int& b) {
-    if((strcmp(a.digits, b.digits)==0) && a.thesign==b.thesign ){
+   if((strcmp(a.digits, b.digits)==0) && a.thesign==b.thesign ){
     return false;
    }
    return true;
 }
 
 bool operator>(const inf_int& a, const inf_int& b) {
+    //부호가 다르면
     if (a.thesign && !b.thesign){
-        return true; //a is positive, b is negative
-    } else if (!a.thesign && b.thesign) {
-        return false;  // a is negative, b is positive
+        return true; //a양수,b음수
+    } 
+    else if(!a.thesign && b.thesign){
+        return false; //a음수, b양수
     }
 
-    if (a.length > b. length){
-        return a.thesign; 
-    }
+    //부호 같으면
+    if(a.length > b.length){
+        return a.thesign; //길이 긴 쪽의 thesign 따라감
+    } 
     else if(a.length < b.length){
-        return !a.thesign;
+       return !a.thesign;
     }
-    else {
+    else{
+        //길이가 같으면, 숫자를 하나하나 비교
         for(unsigned int i = 0; i < a.length; i++){
-            if(a.digits[i]>b.digits[i]){
+            if(a.digits[i] > b.digits[i]){
                 return a.thesign;
             }
-            else if(a.digits[i]<b.digits[i]){
+            else if(a.digits[i] < b.digits[i]){
                 return !a.thesign;
             }
         }
     }
 
+    //두 숫자가 같으면
     return false;
 
 }
 
+//위에 >를 반전시킴
 bool operator<(const inf_int& a, const inf_int& b) {
      if (a.thesign && !b.thesign){
         return false; 
@@ -143,66 +164,121 @@ bool operator<(const inf_int& a, const inf_int& b) {
 }
 
 inf_int operator+(const inf_int& a, const inf_int& b) {
+
+    //두 숫자의 부호가 다른 경우
     if(!a.thesign || !b.thesign){
-        return inf_int();
+       //음수 혼합된 '+'는 뺄셈으로 대체하는 걸로
+       //일단 지금은 구현 안함
+       return inf_int(); // 일시적으로 0 반환
     }
-
+    //결과 저장할 임시 문자열
     char temp[10000];
-    int carry = 0;
-    int i = a.length - 1, j = b.length-1, k = 0;
 
-    while (i >= 0 || j >= 0 || carry) {
+    int carry = 0; //십진수 계산 올림용도
+    int i = a.length - 1;
+    int j = b.length - 1;
+    int k = 0;
+
+    //두 숫자를 뒤에서부터 더해줌
+    while(i >= 0 || j >= 0 || carry) {
         int sum = carry;
-        if (i >= 0) sum += a.digits[i--] - '0';
-        if (j >= 0) sum += b.digits[j--] - '0';
-        temp[k++] = (sum % 10) + '0'; 
-        carry = sum / 10; 
+        if(i>=0){
+            sum += a.digits[i--] - '0'; //sum에 한자리 숫자씩 넣기
+        }
+        if(j>=0){
+            sum += b.digits[j--] - '0';
+        }
+        temp[k++] = (sum % 10) + '0'; //문자로 변환해서 임시 문자열에 저장
+        carry = sum / 10; //올림수 올려놓음
     }
 
-    // turn the digits
+    //결과 문자열을 뒤집기
     for (int l = 0; l < k / 2; l++) {
         char t = temp[l];
         temp[l] = temp[k - l - 1];
         temp[k - l - 1] = t;
     }
-    temp[k] = '\0'; 
+    temp[k] = '\0'; // 문자열 종료
 
-    return inf_int(temp); 
+    return inf_int(temp); //임시 문자열로 객체 생성
 }
 
 inf_int operator-(const inf_int& a, const inf_int& b) {
-
+    
+    //두 숫자의 부호가 다른 경우
     if (!a.thesign || !b.thesign) {
-        
-        return inf_int(); // return 0 temporarily
+        //일단 0 반환
+        //구현 안함
+        return inf_int(); 
     }
 
-    char temp[10000]; // size of storage
+    // 두 수의 크기를 비교하여 a가 b보다 작은지 확인
+    bool aIsSmaller = false;
+    if (a.length < b.length) {
+        aIsSmaller = true;
+    } else if (a.length == b.length) {
+        for (int i = 0; i < a.length; i++) {
+            if (a.digits[i] < b.digits[i]) {
+                aIsSmaller = true;
+                break;
+            } else if (a.digits[i] > b.digits[i]) {
+                break;
+            }
+        }
+    }
 
-    int borrow = 0; 
+    // 결과를 저장할 임시 배열
+    char temp[10000];
+    memset(temp, 0, sizeof(temp)); // 배열을 0으로 초기화
+
+    int borrow = 0; // 빌리는 수
     int i = a.length - 1, j = b.length - 1, k = 0;
 
+    // a가 b보다 작으면, b - a를 계산하고 결과의 부호를 음수로 설정
+    if (aIsSmaller) {
+        // i, j 바꾸기
+        i = b.length - 1;
+        j = a.length - 1;
+    }
+
     while (i >= 0 || j >= 0) {
-        int diff = borrow;
-        if (i >= 0) diff += a.digits[i--] - '0';
-        if (j >= 0) diff -= b.digits[j--] - '0';
+        int a_digit = i >= 0 ? (aIsSmaller ? b.digits[i--] - '0' : a.digits[i--] - '0') : 0;
+        int b_digit = j >= 0 ? (aIsSmaller ? a.digits[j--] - '0' : b.digits[j--] - '0') : 0;
+
+        int diff = a_digit - b_digit - borrow;
         if (diff < 0) {
             diff += 10;
-            borrow = 1; //1을 빌려야 함
+            borrow = 1;
         } else {
             borrow = 0;
         }
-        temp[k++] = diff + '0'; 
+        temp[k++] = diff + '0';
     }
 
+    // 결과 배열의 순서를 뒤집음
     for (int l = 0; l < k / 2; l++) {
         char t = temp[l];
         temp[l] = temp[k - l - 1];
         temp[k - l - 1] = t;
     }
-    temp[k] = '\0'; 
 
-    return inf_int(temp); 
+    // 결과 문자열에서 앞쪽의 불필요한 0 제거
+    int start = 0;
+    while (start < k - 1 && temp[start] == '0') start++;
+    int newSize = k - start;
+    char* result = new char[newSize + 1 + (aIsSmaller ? 1 : 0)]; // '-'를 위한 공간 추가
+
+    if (aIsSmaller) {
+        result[0] = '-';
+        memcpy(result + 1, temp + start, newSize);
+    } else {
+        memcpy(result, temp + start, newSize);
+    }
+    result[newSize + (aIsSmaller ? 1 : 0)] = '\0';
+
+    inf_int resultObj(result);
+    delete[] result; // 동적 할당된 메모리 해제
+    return resultObj;
 }
 
 inf_int operator*(const inf_int& a, const inf_int& b) {
